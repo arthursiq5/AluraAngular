5ec12fd942cc5863eef2539e0eee4dfe1bdefef9
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-30 15:32:02 -0300

    adicionado import para HttpClient

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     PhotosModule
   ],
