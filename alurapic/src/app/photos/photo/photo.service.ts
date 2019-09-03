4cf2960f681080895530494875c2410f523d3bfc
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-03 15:57:55 -0300

    Componente "PhotoListResolver" agora entrega as fotos do banco Usando "PhotoListResolver" para trazer os dados do banco já resolvidos para o PhotoListComponent apresentar; PhotoListComponent atrelado intimamente à rota "user/:userName" PhotoListComponent agora é responsável apenas por mostrar os dados recebidos, e não mais por convertê-los;

--- alurapic/src/app/photos/photo/photo.service.ts
+++ alurapic/src/app/photos/photo/photo.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Photo } from "./photo";
+import { Observable } from "rxjs";
 
 const api = 'http://localhost:3000';
 @Injectable({
@@ -9,7 +10,7 @@ const api = 'http://localhost:3000';
 export class PhotoService{
   constructor(private http: HttpClient){}
 
-  listFromUser(userName:string){
+  listFromUser(userName:string):Observable<Photo[]>{
     return this.http.get<Photo[]>
           (api+"/"+userName+"/photos");
   }
