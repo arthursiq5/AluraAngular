b8ae02eb39ab27eced6b45751652409e16a60d99
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-04 17:13:16 -0300

    diretiva escurece o componente hospedeiro quando mouse está focado nele

--- alurapic/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ alurapic/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,8 +1,24 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, HostListener, Renderer } from "@angular/core";
 
 @Directive({
   selector: "[apDarkenOnHover]"
 })
 export class DarkenOnHoverDirective{
 
+  constructor(
+    private element: ElementRef,
+    private render: Renderer
+  ){ }
+
+  @HostListener('mouseover')
+  darkenOn(){
+    this.render.setElementStyle(
+      this.element.nativeElement, 'filter', 'brightness(70%)')
+  }
+
+  @HostListener('mouseleave')
+  darkenOut(){
+    this.render.setElementStyle(
+      this.element.nativeElement, 'filter', 'brightness(100%)')
+  }
 }
--- alurapic/src/app/shared/directives/darken-on-hover/darken-on-hover.module.ts
+++ alurapic/src/app/shared/directives/darken-on-hover/darken-on-hover.module.ts
@@ -5,4 +5,4 @@ import { DarkenOnHoverDirective } from "./darken-on-hover.directive";
   declarations: [ DarkenOnHoverDirective ],
   exports: [ DarkenOnHoverDirective ]
 })
-export class DarkenOnHoverModule { }
+export class DarkenOnHoverModule {}
