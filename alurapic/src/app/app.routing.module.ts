75dc87af6b8daf24027a60ce3cc4253be078bcba
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-05 13:49:35 -0300

    Página de login agora indexada no arquivo de rotas

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -5,6 +5,7 @@ import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
 import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
+import { SignInComponent } from "./home/signin/signin.component";
 
 const routes:Routes = [
   {
@@ -14,6 +15,7 @@ const routes:Routes = [
       photos: PhotoListResolver
     }
   },
+  { path:'', component:SignInComponent },
   { path:'p/add', component:PhotoFormComponent },
   { path:'**', component:NotFoundComponent },
 ];
