c991a7892d9fac550edeea4aa9d9eb14fd4a484f
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-06 17:31:15 -0300

    Criado componente para cadastro de usuários

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -7,6 +7,7 @@ import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
 import { SignInComponent } from "./home/signin/signin.component";
 import { AuthGuard } from "./core/auth/auth.guard";
+import { SignUpComponent } from "./home/signup/signup.component";
 
 const routes:Routes = [
   {
@@ -20,6 +21,10 @@ const routes:Routes = [
     path:'',
     component:SignInComponent,
     canActivate: [AuthGuard]
+  },
+  {
+    path:'signup',
+    component:SignUpComponent
   },
   {
     path:'p/add',
