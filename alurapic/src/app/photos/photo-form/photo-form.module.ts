7afc4469941fed66acb7f62a5977e134e2a5faa9
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-10 17:23:33 -0300

    Usando diretiva ImmediateClick na inicialização da página Botão de escolha de fotos é clicado automaticamente ao carregar página

--- alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -6,6 +6,7 @@ import { PhotoFormComponent } from "./photo-form.component";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { VMessageModule } from "../../shared/components/vmessage/vmessage.module";
 import { PhotoModule } from "../photo/photo.module";
+import { ImmediateClickModule } from "../../shared/directives/immediate-click/immediate-click.module";
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { PhotoModule } from "../photo/photo.module";
     ReactiveFormsModule,
     VMessageModule,
     RouterModule,
-    PhotoModule
+    PhotoModule,
+    ImmediateClickModule
   ]
 })
 export class PhotoFormModule{ }
