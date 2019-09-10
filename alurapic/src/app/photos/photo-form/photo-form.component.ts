9274c557e44e4ea0ca423003e0fff45004a4b03a
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-10 15:40:27 -0300

    Validando o formulário e o preparando para o envio

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ap-photo-form',
   templateUrl: './photo-form.component.html',
   styleUrls: ['./photo-form.component.css']
 })
-export class PhotoFormComponent { }
+export class PhotoFormComponent implements OnInit {
+
+  photoForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){ }
+
+  ngOnInit(): void {
+    this.photoForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true]
+    })
+  }
+}
