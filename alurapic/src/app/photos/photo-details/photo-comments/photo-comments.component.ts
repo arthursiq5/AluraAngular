3299fd8a35700247f2b6ec5cae93973494ca4208
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-11 16:55:12 -0300

    Mostrando os dados dinamicamente ao salvar comentário

--- alurapic/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ alurapic/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
+import { switchMap, tap } from 'rxjs/operators'
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { PhotoComment } from "../../photo/photo-comment";
 import { PhotoService } from "../../photo/photo.service";
@@ -27,13 +28,16 @@ export class PhotoCommentsComponent implements OnInit{
     });
   }
 
-  save(){
+  save(): void{
     const comment: string = this.commentForm.get('comment').value as string;
-    this.photoService
+    this.comments$ = this.photoService
         .addComment(this.photoId, comment)
-        .subscribe(() => {
+        .pipe(switchMap(() =>
+          this.photoService.getComments(this.photoId)
+        ))
+        .pipe(tap(() => {
           this.commentForm.reset();
-          alert('Comentário adicionado com sucesso')
-        });
+          alert('Comentário adicionado com sucesso');
+        }));
   }
 }
