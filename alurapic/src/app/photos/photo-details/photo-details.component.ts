50d980230df29d0945e3f9e24f81f29d2e1587dc
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-11 15:26:25 -0300

    Lendo dados das fotos em modo assíncrono

--- alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
 import { PhotoService } from "../photo/photo.service";
 import { Photo } from "../photo/photo";
 
@@ -8,7 +9,7 @@ import { Photo } from "../photo/photo";
 })
 export class PhotoDetailsComponent implements OnInit{
 
-  photo: Photo;
+  photo$: Observable<Photo>;
 
   constructor(
     private route: ActivatedRoute,
@@ -16,11 +17,8 @@ export class PhotoDetailsComponent implements OnInit{
   ){ }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params.photoId;
-    this.photoService
-        .findById(id)
-        .subscribe(
-          photo => this.photo = photo
-        );
+    this.photo$ = this.photoService.findById(
+      this.route.snapshot.params.photoId
+    );
   }
 }
