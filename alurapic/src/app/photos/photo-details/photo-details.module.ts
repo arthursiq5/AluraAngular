6575835ad8b86649dcf7097bc98023cf0066650b
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-11 16:23:40 -0300

    Configurada validação do formulário de mensagens

--- alurapic/src/app/photos/photo-details/photo-details.module.ts
+++ alurapic/src/app/photos/photo-details/photo-details.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { PhotoDetailsComponent } from "./photo-details.component";
 import { PhotoModule } from "../photo/photo.module";
 import { PhotoComponent } from "../photo/photo.component";
 import { PhotoCommentsComponent } from "./photo-comments/photo-comments.component";
+import { VMessageModule } from "../../shared/components/vmessage/vmessage.module";
 
 
 @NgModule({
@@ -17,9 +18,11 @@ import { PhotoCommentsComponent } from "./photo-comments/photo-comments.componen
   exports: [ PhotoDetailsComponent],
   imports: [
     CommonModule,
-    FormsModule,
     PhotoModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    VMessageModule
   ]
 })
 export class PhotoDetailsModule{ }
