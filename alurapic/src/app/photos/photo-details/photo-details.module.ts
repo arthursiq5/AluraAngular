291dd1c5e707a816f3f3cb5a041fe34a742f555e
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-11 15:21:40 -0300

    Mostrando dados das fotos do banco

--- alurapic/src/app/photos/photo-details/photo-details.module.ts
+++ alurapic/src/app/photos/photo-details/photo-details.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { PhotoDetailsComponent } from "./photo-details.component";
+import { FormsModule } from "@angular/forms";
+import { PhotoModule } from "../photo/photo.module";
+import { PhotoComponent } from "../photo/photo.component";
 
 
 @NgModule({
   declarations: [ PhotoDetailsComponent ],
   exports: [ PhotoDetailsComponent],
-  imports: [ CommonModule ]
+  imports: [
+    CommonModule,
+    FormsModule,
+    PhotoModule
+  ]
 })
 export class PhotoDetailsModule{ }
