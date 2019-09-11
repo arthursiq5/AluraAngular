26255bbc161223305ba9e9ed6245527cfbc525dd
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-11 14:07:42 -0300

    protegendo rota 'p/add'

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -6,6 +6,7 @@ import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
 import { AuthGuard } from "./core/auth/auth.guard";
+import { RequiresAutenticationGuard } from "./core/auth/requires-autentication.guard";
 
 const routes:Routes = [
   {
@@ -26,7 +27,8 @@ const routes:Routes = [
   },
   {
     path:'p/add',
-    component:PhotoFormComponent
+    component:PhotoFormComponent,
+    canActivate: [RequiresAutenticationGuard]
   },
   {
     path:'**',
