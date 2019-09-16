c336423f5cead72adf3a48e3e70b9858c9ab220d
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-16 14:50:19 -0300

    Preparando ambiente para envio de dados ao servidor injector ajuda a injetar dependências dinamicamente; fazendo log de objeto com as propriedades do erro, incluindo URL e username

--- alurapic/src/app/errors/global-error-handler/global-error-handler.ts
+++ alurapic/src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,17 +1,44 @@
-import { ErrorHandler } from "@angular/core";
+import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { LocationStrategy, PathLocationStrategy } from "@angular/common";
 import * as StackTrace from 'stacktrace-js';
+import { UserService } from "../../core/user/user.service";
 
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
+
+  constructor(private injector: Injector){}
 
   handleError(error: any): void {
     console.log('passei pelo handler');
+
+    const location = this.injector.get(LocationStrategy);
+    const userService = this.injector.get(UserService);
+
+    const url = location instanceof PathLocationStrategy
+                 ? location.path()
+                 : '';
+
+
+    const message = error.message
+                      ? error.message
+                      : error.toString();
+
     StackTrace
       .fromError(error)
       .then(stackFrames => {
-        const stackAsString = stackFrames.map(
-          stackFrame => stackFrame.toString())
-          .join('\n');
+        const stackAsString = stackFrames
+                                .map(stackFrame => stackFrame.toString())
+                                .join('\n');
+
+        console.log(message);
         console.log(stackAsString)
-      })
+
+        console.log({
+          message,
+          url,
+          userName: userService.getUserName(),
+          stack: stackAsString
+        });
+      });
   }
 }
