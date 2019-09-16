f4ef9f53369713acbf3d943de5b1612c2734c0af
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-16 14:13:05 -0300

    Substituição do handler padrão de erro para padronizar mensagens de erro

--- alurapic/src/app/errors/errors.module.ts
+++ alurapic/src/app/errors/errors.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
 
 @NgModule({
   imports: [
@@ -9,6 +10,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   declarations: [
     NotFoundComponent
+  ],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler
+    }
   ]
 })
 export class ErrorsModule { }
