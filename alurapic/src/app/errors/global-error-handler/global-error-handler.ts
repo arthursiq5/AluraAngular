2f9c3bde12925e6dec149a7225a8d4f767c809ef
Arthur Siqueira <arthur.siq5@gmail.com>
2019-09-16 14:29:16 -0300

    Modificando mensagem de erro com StackTrace Percorrendo array de erros e concatenando dados de erros

--- alurapic/src/app/errors/global-error-handler/global-error-handler.ts
+++ alurapic/src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,9 +1,17 @@
 import { ErrorHandler } from "@angular/core";
+import * as StackTrace from 'stacktrace-js';
 
 export class GlobalErrorHandler implements ErrorHandler {
 
   handleError(error: any): void {
     console.log('passei pelo handler');
-    throw error;
+    StackTrace
+      .fromError(error)
+      .then(stackFrames => {
+        const stackAsString = stackFrames.map(
+          stackFrame => stackFrame.toString())
+          .join('\n');
+        console.log(stackAsString)
+      })
   }
 }
